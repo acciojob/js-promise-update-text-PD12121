750aed5924246de11c0b76183e8396ebbdeb7669
PD12121 <115214172+PD12121@users.noreply.github.com>
2023-07-28 19:42:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function updateElementText() {
+	return new Promise((resolve) =>{
+		setTimeout(() =>{
+			resolve("Hello, World");
+		},1000);
+	});
+}
+const outputElement = document.getElementById("output");
+updateElementText().then((text) =>{
+	outputElement.textContent = text;
+});
